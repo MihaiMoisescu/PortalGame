8693a930317909edbafb25e34ef44f59b4b2c8ae
Beatrice Moraru <morarubeatrice77@yahoo.com>
2024-12-10 23:37:50 +0200

    Update movement to use CharacterController

--- PortalGame/Assets/Scripts/PlayerMovement.cs
+++ PortalGame/Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,60 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private InputAction MoveAction;
-    [SerializeField] private float _movementSpeed = 3.0f;
-    private Transform _transform;
+    [Header("Movement Settings")]
+    public float speed = 5.0f;
+    public float jumpHeight = 2.0f;
+    public float gravity = -9.81f;
+
+    [Header("Mouse Settings")]
+    public float mouseSensitivity = 250f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    private Transform cameraTransform;
+    private float xRotation = 0f;
+
     void Start()
     {
-        _transform = gameObject.GetComponent<Transform>();
-        MoveAction.Enable();
+        controller = GetComponent<CharacterController>();
+        cameraTransform = Camera.main.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 move = MoveAction.ReadValue<Vector3>();
-        Debug.Log(move);
-        Vector3 position = (Vector3)_transform.position + move * _movementSpeed * Time.deltaTime;
-        _transform.position = position;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
