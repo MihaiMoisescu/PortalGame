d1882f8fce711e12f87bae6c0e1b1eb0de70dac6
MihaiMoisescu <mihaimoisescu9@gmail.com>
2025-01-06 21:31:02 +0200

    Adding Starting scene, select level scene and controls scene and made connection between them

---
+++ PortalGame/Assets/Scripts/MenuInputHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInputHandler : MonoBehaviour
+{
+    [SerializeField] private string _MenuSceneName;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (_MenuSceneName != SceneManager.GetActiveScene().name)
+            {
+                SceneManager.LoadScene(_MenuSceneName);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
