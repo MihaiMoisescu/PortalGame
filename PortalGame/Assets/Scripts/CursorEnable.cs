07e7812fade2f73808b136cd70f0f851302e22e1
MihaiMoisescu <mihaimoisescu9@gmail.com>
2025-01-07 16:40:40 +0200

    Added  script to enable the cursor when return to select level scene and adding sound during all game

---
+++ PortalGame/Assets/Scripts/CursorEnable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CursorEnable : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
---
+++ PortalGame/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Face obiectul persistent între scene
+        }
+        else
+        {
+            Destroy(gameObject); // Evită duplicarea obiectului la revenirea în scenă
+        }
+    }
+}
