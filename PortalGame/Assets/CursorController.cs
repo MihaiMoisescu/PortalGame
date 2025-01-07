248200317734b50540138d9a67ae8442c64f986f
MihaiMoisescu <mihaimoisescu9@gmail.com>
2025-01-07 16:48:12 +0200

    Fix buttons on select lvl screen

---
+++ PortalGame/Assets/CursorController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
